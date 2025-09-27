ba6bbdee8b0dd1d422e7eca843e13cbb9ae6f1e4
ExtraQ <31879076+extranquil@users.noreply.github.com>
2025-09-27 15:47:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,37 +27,6 @@ headers = {'User-Agent': 'MiFit/5.3.0 (iPhone; iOS 14.7.1; Scale/3.00)'}
 
 
 # 获取区域天气情况
-def getWeather():
-    if area == "NO":
-        print(area == "NO")
-        return
-    else:
-        global K, type
-        url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + area
-        hea = {'User-Agent': 'Mozilla/5.0'}
-        r = requests.get(url=url, headers=hea)
-        if r.status_code == 200:
-            result = r.text
-            res = json.loads(result)
-            if "多云" in res['data']['forecast'][0]['type']:
-                K = K_dict["多云"]
-            elif "阴" in res['data']['forecast'][0]['type']:
-                K = K_dict["阴"]
-            elif "小雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["小雨"]
-            elif "中雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["中雨"]
-            elif "大雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["大雨"]
-            elif "暴雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["暴雨"]
-            elif "大暴雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["大暴雨"]
-            elif "特大暴雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["特大暴雨"]
-            type = res['data']['forecast'][0]['type']
-        else:
-            print("获取天气情况出错")
 
 
 # 获取北京时间确定随机步数&启动主函数
@@ -67,8 +36,6 @@ def getBeijinTime():
     type = ""
     hea = {'User-Agent': 'Mozilla/5.0'}
     url = r'https://apps.game.qq.com/CommArticle/app/reg/gdate.php'
-    if open_get_weather == "True":
-        getWeather()
     r = requests.get(url=url, headers=hea)
     if r.status_code == 200:
         result = r.text
